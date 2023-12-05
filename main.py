8cdd6f8c8de735e00c1075c4a156c0d2cc5eed61
Niedzwiedz1 <niewadzilukasz@gmail.com>
2023-12-05 12:15:25 +0000

    add multiple pages

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for index, row in df.iterrows():
     topic = row["Topic"]
+    pages = row["Pages"]
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=15)
-    pdf.cell(w=0, h=10, txt=topic, align="L", ln=1)
     pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=10, txt=topic, align="L", ln=1)
     pdf.line(10, 19, 200, 19)
+
+    for i in range(pages-1):
+        pdf.add_page()
+
 
 pdf.output("output.pdf")
